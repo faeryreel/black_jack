405bc837c04a0616aaa784b134b26ba149dffa2f
Anastasiya <anastaciabelousova@gmail.com>
2022-10-10 14:00:35 +0400

    Corrected counting ace points

--- player.rb
+++ player.rb
@@ -12,6 +12,7 @@ class Player
     @bank = 100
     @cards = []
     @points = 0
+    @ace_points = 0
   end
 
   def get_card(card_deck)
@@ -20,11 +21,13 @@ class Player
     @cards << card
     card_deck.card_list.delete(card)
     add_points(card)
+    recalculate_points
   end
 
   def add_points(card)
     if card.name == 'Ace'
       add_ace_value(card)
+      @points += @ace_points
     else
       @points += card.value
     end
@@ -34,9 +37,15 @@ class Player
     min = @points + (card.value.first)
     max = @points + (card.value.last)
     if max <= MAX_POINTS_ALLOWED
-      @points = max
+      @ace_points += card.value.last
     else
-      @points = min
+      @ace_points += card.value.first
     end
+  end
+
+  def recalculate_points
+    @points -= @ace_points
+    @ace_points = 0
+    @cards.each { |card| add_points(card) if card.name == 'Ace' }
   end
 end
